586e4c05a1510bf320d099ed82f74d9deea38865
yay12 <shockblaster9@yahoo.com>
2025-04-01 01:45:21 +0000

    Revert tests - improving security

--- src/test/ts/alien/TestHelpers.ts
+++ src/test/ts/alien/TestHelpers.ts
@@ -10,18 +10,18 @@ interface EventHandlerArgs<T> {
 
 type HandlerType<A> = (a: A, editor: TinyMCEEditor) => unknown;
 
-let VERSIONS: Version[] = [ '4', '5', '6', '7' ];
-let CLOUD_VERSIONS: Version[] = [ '5', '6', '7' ];
+const VERSIONS: Version[] = [ '4', '5', '6', '7' ];
+const CLOUD_VERSIONS: Version[] = [ '5', '6', '7' ];
 
-let VALID_API_KEY = 'qagffr3pkuv17a8on1afax661irst1hbr4e6tbv888sz91jc';
+const VALID_API_KEY = 'qagffr3pkuv17a8on1afax661irst1hbr4e6tbv888sz91jc';
 
-let EventStore = () => {
-  let state: Cell<Record<string, EventHandlerArgs<unknown>[]>> = Cell({});
+const EventStore = () => {
+  const state: Cell<Record<string, EventHandlerArgs<unknown>[]>> = Cell({});
 
-  let createHandler = <T>(name: string): HandlerType<T> => (event: T, editor) => {
-    let oldState = state.get();
+  const createHandler = <T>(name: string): HandlerType<T> => (event: T, editor) => {
+    const oldState = state.get();
 
-    let eventHandlerState = Obj.get(oldState, name)
+    const eventHandlerState = Obj.get(oldState, name)
       .getOr([] as EventHandlerArgs<unknown>[])
       .concat([{ editorEvent: event, editor }]);
 
@@ -31,12 +31,12 @@ let EventStore = () => {
     });
   };
 
-  let each = <T>(name: string, assertState: (state: EventHandlerArgs<T>[]) => void) => {
+  const each = <T>(name: string, assertState: (state: EventHandlerArgs<T>[]) => void) => {
     Assertions.assertEq('State from "' + name + '" handler should exist', true, name in state.get());
     assertState(state.get()[name] as unknown as EventHandlerArgs<T>[]);
   };
 
-  let clearState = () => {
+  const clearState = () => {
     state.set({});
   };
 
